c95fbe5d257aa1e761cd15cd0284dbc82b5dee8e
ubcse <jxboulwa@gmail.com>
2024-11-13 23:46:41 -0500

    adjusted frames

--- xxx_game/views.py
+++ xxx_game/views.py
@@ -15,14 +15,15 @@ import logging
 mongo_client = MongoClient("mongo")
 from bson.objectid import ObjectId
 from django.shortcuts import redirect
+import json
 
 # databases
 db = mongo_client["webapp"]
 user_collection = db["users"]
 game_collection = db["games"]
 game_post_collection = db["game_posts"]
-
 game_user_collection = db["game_users"]
+guest_collection = db["guests"]
 
 
 global_salt = b'$2b$12$ldSsU24BK6EPANRbUpvXRu'
@@ -96,8 +97,9 @@ def login(request):
 
         response = redirect("/", {"username" : username})
         response.set_cookie("auth_token", auth_token, max_age=60*60*24, httponly=True)
-
         response.set_cookie("alert-info", "login success")
+        if request.COOKIES.get('guest'):
+            response.delete_cookie('guest')
         return response
 
     response = redirect("index")
@@ -142,15 +144,19 @@ def logout(request):
     # Hash token and check for it in the database
     auth_token_hash = bcrypt.hashpw(auth_token.encode(), global_salt) # Hash the unhashed cookie auth token so we can check for it in the DB
     user_collection.update_one({ "auth_token_hash": auth_token_hash}, {"$unset": {"auth_token_hash": ""}}) # Delete auth token field from DB
-    return redirect("/")
+    response = redirect("/")
+    response.delete_cookie("auth_token") # Delete the cookie
+    return response
 
 #createpost
 ## function to create post object
 def create_post(request):
     user = get_user_from_auth(request)
 
-    content = request.POST.get('content')    
-    escaped_content = escape_HTML(content)
+    content = request.POST.get('content')
+    # When replacing the Django template, a function similar to escape_html will be used for its value, so there is no need to use ? ???  
+    # escaped_content = escape_HTML(content)
+    escaped_content = content
     feeling = request.POST.get('feeling')
     if user:
         post = { 
@@ -163,18 +169,52 @@ def create_post(request):
     if not user:
         post = { 
         'username': "Guest",
-        'content':content,
+        'content':escaped_content,
         'feeling': feeling,
         'likes': []
      }
     
     db['posts'].insert_one(post)
     return redirect ("/chat")
+
+def chat_list(request):
+    user = get_user_from_auth(request)
+  
+    posts = list(db['posts'].find())
+    for post in posts:
+        post['_id'] = str(post['_id'])
+        post['post_id'] = post['_id']
+
+    return JsonResponse(posts,safe=False)
+
+def like_posts_ajax(request,post_id):
+    user = get_user_from_auth(request)
+    if user is None:
+        response = JsonResponse({'code':100,'status': 'not logged in'})
+        response.set_cookie('alert-info', 'You are not logged in')
+        return response
     
+    post = db['posts'].find_one({'_id': ObjectId(post_id)})
+    if not post:
+        response = JsonResponse({'code':101,'status': 'post not found'})
+        response.set_cookie('alert-info', 'Post not found')
+        return response
+
+    if user['username'] not in post.get('likes', []):
+        db['posts'].update_one(
+            {'_id': ObjectId(post_id)},
+            {'$push':{'likes': user['username']}}
+        )
+        return JsonResponse({'code':0,'status': 'success'})
+    
+    db['posts'].update_one(
+        {'_id':ObjectId(post_id)},
+        {'$pull':{'likes': user['username']}}
+    )
+    return JsonResponse({'code':0,'status': 'success'})
+
 def chat(request):
     user = get_user_from_auth(request)
-    if not user:
-        response = redirect('/')
         
     posts = list(db['posts'].find())
     for post in posts:
@@ -214,55 +254,100 @@ def like_posts(request,post_id):
 
 def game_lobby(request):
     user = get_user_from_auth(request)
+    create_guest_id = False
 
     context = { 
         "username":user.get('username') if user else "Guest",
         "logged_in": user is not None, # used to determine if the user is logged in or not
         'avatar_url': user.get('avatar') if user and user.get('avatar') else 'avatar/default.png',
     }
+    logging.warning('user:'+str(user))
 
-    if user is not None:
-        # check if user is already in a game, if so, redirect to the game room
-        user_game = game_user_collection.find_one({'username': user['username']})
-        if user_game is not None:
-            game = game_collection.find_one({'_id': ObjectId(user_game['game_id'])})
-            if game is not None:
-                return redirect('game_room/'+str(game['_id']))
-    else:
-        response = redirect('index')
-        response.set_cookie('alert-info', 'Guest cannot join game, please login')
-        return redirect('index')
+    if user is None:
+        guest_id = generate_guest_id()
+        create_guest_id = True
+        
+    # if user is not None:
+    #     # check if user is already in a game, if so, redirect to the game room
+    #     user_game = game_user_collection.find_one({'username': user['username']})
+    #     if user_game is not None:
+    #         game = game_collection.find_one({'_id': ObjectId(user_game['game_id'])})
+    #         if game is not None:
+    #             return redirect('game_room/'+str(game['_id']))
+    # else:
+    #     guest_id = request.COOKIES.get('guest')
+        
+    #     if guest_id is None:
+    #         guest_id = generate_guest_id()
+    #         create_guest_id = True
 
-    return render(request, 'xxx_game/game_lobby.html',context)   
+    #     user_game = game_user_collection.find_one({'guest_id': guest_id})
+    #     logging.warning(user_game)
+    #     if user_game is not None:
+    #         game = game_collection.find_one({'_id': ObjectId(user_game['game_id'])})
+    #         if game is not None:
+    #             return redirect('game_room/'+str(game['_id']))
+            
+    response = render(request, 'xxx_game/game_lobby.html',context)
+    if create_guest_id:
+        response.set_cookie('guest', guest_id)
+    return response
+
+def generate_guest_id():
+    guest_id = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+    guest_id = 'guest_'+guest_id
+    return guest_id
 
 # path /game_room/<game_id> 
 # User join the game, and one user can only join one game at a time.
 def game_room(request,id):
     user = get_user_from_auth(request)
+    if user is None:
+        guest = request.COOKIES.get('guest',generate_guest_id())
+
 
     game = game_collection.find_one({'_id': ObjectId(id)})
     if game is None:
-        game_user_collection.delete_one({'username': user['username']})
         return redirect('game_lobby')
     
-    game_user = game_user_collection.find_one({'game_id':id, 'username': user['username']})
-    if game_user is not None and ( game['status'] == 'playing' or game['status'] == 'finished'):
-        # user is already in the game, redirect to the game
-        return redirect('/game/'+str(game['_id']))
+        # if user is not None:
+        #     game_user_collection.delete_one({'username': user['username']})
+        #     return redirect('game_lobby')
+        # else:
+        #     game_user_collection.delete_one({'guest_id': guest})
+        #     return redirect('game_lobby')
 
-    if game['status'] != 'waiting':
-        response = redirect('game_lobby')
-        response.set_cookie('alert-info', 'Game already started')
-        return response
+    # if user is not None:
+    #     game_user = game_user_collection.find_one({'game_id':id, 'username': user['username']})
+    # else:
+    #     game_user = game_user_collection.find_one({'game_id':id, 'guest_id': guest})
+
+    # if game_user is not None and ( game['status'] == 'playing' or game['status'] == 'finished'):
+    #     # user is already in the game, redirect to the game
+    #     return redirect('/game/'+str(game['_id']))
+
+    # if game['status'] != 'waiting':
+    #     response = redirect('game_lobby')
+    #     response.set_cookie('alert-info', 'Game already started')
+    #     return response
     
-    if user['username'] not in game['players']:
+    if user and user['username'] not in game['players']:
         # add user to the game
         game['players'].append(user['username'])
         game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'players': game['players']}})
+    
+    if user is None and guest not in game['players']:
+        # add guest to the game
+        game['players'].append(guest)
+        game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'players': game['players']}})
 
-    if game_user_collection.find_one({'username': user['username']}) is None:
-        # add user to the game_user collection
-        game_user_collection.insert_one({'game_id': id, 'username': user['username']})
+    # if user and game_user_collection.find_one({'username': user['username']}) is None:
+    #     # add user to the game_user collection
+    #     game_user_collection.insert_one({'game_id': id, 'username': user['username']})
+    
+    # if user is None and game_user_collection.find_one({'guest_id': guest}) is None:
+    #     # add guest to the game_user collection
+    #     game_user_collection.insert_one({'game_id': id, 'guest_id': guest})
     
 
     context = { 
@@ -276,7 +361,8 @@ def game_room(request,id):
 
     return render(request, 'xxx_game/game_room.html',context)
 
-# path /game
+# path /game/
+# to start game
 def game(request,id):
     user = get_user_from_auth(request)
     context = { 
@@ -290,6 +376,7 @@ def game(request,id):
         response = redirect('game_lobby')
         response.set_cookie('alert-info', 'Game not found')
         return response
+    
     if game['status'] == 'waiting':
         game['status'] = 'playing'
         game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'status': 'playing'}})
@@ -369,7 +456,7 @@ def create_game(request):
 
     game = {
         'created_by': user['username'],
-        'players': [user['username']],
+        'players': [],
         'status': 'waiting', # waiting, playing, finished
         'name': name,
         'join-code': join_code,
@@ -378,7 +465,7 @@ def create_game(request):
     game = game_collection.insert_one(game)
 
 
-    response = redirect('/game_room/'+str(game.inserted_id))
+    response = redirect('game_lobby')
     response.set_cookie('alert-info', 'Game created')
     return response
 
@@ -403,7 +490,10 @@ def get_game_player(request,id):
     player_list = []
     for player in players:
         player = user_collection.find_one({'username': player})
-        player_list.append({'avatar':player.get('avatar') if player.get('avatar') else 'avatar/default.png', 'username': player['username']})
+        if player:
+            player_list.append({'avatar':player.get('avatar') if player.get('avatar') else 'avatar/default.png', 'username': player['username']})
+        else:
+            player_list.append({'avatar':'avatar/default.png', 'username': 'Guest'})
 
     response = JsonResponse(player_list, safe=False)
     return response
@@ -412,30 +502,28 @@ def get_game_player(request,id):
 def game_chat(request):
     user = get_user_from_auth(request)
 
-    if not user:
-        response = redirect('/')
-        
-    posts = list(db['game_posts'].find())
-    for post in posts:
-        post['post_id'] = str(post['_id'])
-
-    if request.POST.get('message') is None:
-        return JsonResponse({'status': 'no message'})
-    
-    if request.POST.get('message') == '':
-        return JsonResponse({'status': 'empty message'})
-    
-    message = escape_HTML(request.POST.get('message'))
-
-    post = {
-        'game_id': request.POST.get('game_id'),
+    payload = json.loads(request.body)
+    content = payload.get('content')    
+    escaped_content = escape_HTML(content)
+    feeling = payload.get('feeling')
+    if user:
+        post = { 
         'username': user['username'],
-        'message': message,
-    }    
-
-    game_post_collection.insert_one(post)
-
-    return redirect('/game/'+request.POST.get('game_id'))
+        'content':escaped_content,
+        'feeling': feeling,
+        'likes': []
+       
+     }
+    if not user:
+        post = { 
+        'username': "Guest",
+        'content':escaped_content,
+        'feeling': feeling,
+        'likes': []
+     }
+    
+    db['posts'].insert_one(post)
+    return JsonResponse({'code':0,'status': 'success'})
 
 # path /game_chat_list/<game_id>
 def game_chat_list(request,id):
@@ -459,78 +547,83 @@ def game_chat_list(request,id):
 # code 102: game already started
 def leave_game(request,id):
     user = get_user_from_auth(request)
-    if not user:
-        response = JsonResponse({'code':100,'status': 'not logged in'})
-        response.set_cookie('alert-info', 'You are not logged in')
-        return response
-    
-    game_user = game_user_collection.find_one({'game_id':id, 'username': user['username']})
 
-    logging.warning('game_user:'+str(game_user))
-    if game_user is None:
-        response = JsonResponse({'code':101,'status': 'not in the game'})
-        response.set_cookie('alert-info', 'You are not in the game')
-        return response
+    # if not user:
+    #     response = JsonResponse({'code':100,'status': 'not logged in'})
+    #     response.set_cookie('alert-info', 'You are not logged in')
+    #     return response
+    
+    # game_user = game_user_collection.find_one({'game_id':id, 'username': user['username']})
+
+    # if game_user is None:
+    #     response = JsonResponse({'code':101,'status': 'not in the game'})
+    #     response.set_cookie('alert-info', 'You are not in the game')
+    #     return response
 
     game = game_collection.find_one({'_id': ObjectId(id)})
-    logging.warning('game:'+str(game))
     if game is not None:
-        if game['status'] != 'waiting' and game['status'] != 'finished':
-            response = JsonResponse({'code':102,'status': 'game already started'})
-            response.set_cookie('alert-info', 'Game already started , only waiting and finished game can be left')
-            return response
-        
-        if user['username'] in game['players']:
-            game['players'].remove(user['username'])
-            game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'players': game['players']}})
+        # if game['status'] != 'waiting' and game['status'] != 'finished':
+        #     response = JsonResponse({'code':102,'status': 'game already started'})
+        #     response.set_cookie('alert-info', 'Game already started , only waiting and finished game can be left')
+        #     return response
+        if user is not None:
+            if user['username'] in game['players']:
+                game['players'].remove(user['username'])
+                game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'players': game['players']}})
+
+        else:
+            guest = request.COOKIES.get('guest',None)
+            if guest is not None and guest in game['players']:
+                game['players'].remove(guest)
+                game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'players': game['players']}})
     
-    game_user_collection.delete_one({'game_id': id, 'username': user['username']})
+    # game_user_collection.delete_one({'game_id': id, 'username': user['username']})
 
 
     response = JsonResponse({'code':0,'status': 'success'})
     return response
 
 # path /finish_game/<game_id>
-def finish_game(request,id):
-    game = game_collection.find_one({'_id': ObjectId(id)})
-    if game is None:
+# def finish_game(request,id):
+#     game = game_collection.find_one({'_id': ObjectId(id)})
+#     if game is None:
         
-        response = redirect('game_lobby')
-        response.set_cookie('alert-info', 'Game not found')
-        return response
+#         response = redirect('game_lobby')
+#         response.set_cookie('alert-info', 'Game not found')
+#         return response
 
-    logging.warning('game'+str(game))
-    game['status'] = 'finished'
-    logging.warning(game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'status': 'finished'}}))
-    logging.warning('game'+str(game_collection.find_one({'_id': ObjectId(id)})))
+#     logging.warning('game'+str(game))
+#     game['status'] = 'finished'
+#     logging.warning(game_collection.update_one({'_id': ObjectId(id)}, {'$set': {'status': 'finished'}}))
+#     logging.warning('game'+str(game_collection.find_one({'_id': ObjectId(id)})))
 
-    response = redirect('/game/'+str(game['_id']))
-    response.set_cookie('alert-info', 'Game finished')
-    return response
+#     response = redirect('/game/'+str(game['_id']))
+#     response.set_cookie('alert-info', 'Game finished')
+#     return response
 
 # path /join_game/<join_code>
 def join_game(request,join_code):
     user = get_user_from_auth(request)
-    logging.warning('user:'+str(user))
-    if not user:
-        response = redirect('game_lobby')
-        response.set_cookie('alert-info', 'You are not logged in')
-        return response
+
+    # if not user:
+    #     response = redirect('game_lobby')
+    #     response.set_cookie('alert-info', 'You are not logged in')
+    #     return response
     
     game = game_collection.find_one({'join-code': join_code})
     if game is None:
         logging.warning('game not found')
-        response = redirect('game_lobby')
+        response = JsonResponse({'code':100,'status': 'Game not found'})
         response.set_cookie('alert-info', 'Game not found')
         return response
     
-    if game['status'] != 'waiting':
-        logging.warning('game already started')
-        response = redirect('game_lobby')
-        response.set_cookie('alert-info', 'Game already started')
-        return response
+    # if game['status'] != 'waiting':
+    #     logging.warning('game already started')
+    #     response = redirect('game_lobby')
+    #     response.set_cookie('alert-info', 'Game already started')
+    #     return response
     
-    response = redirect('/game_room/'+str(game['_id']))
+    response = JsonResponse({'code':0,'status': 'success','game_id': str(game['_id'])})
     return response
 
 # path /check_game_start/<game_id>
